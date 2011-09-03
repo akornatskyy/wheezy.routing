7f5030ed2814e4e2c05d46f25acd3e462fd96a5b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-03 20:26:06 +0300

    Added a simple wsgi 'helloworld' application.

---
+++ demos/hello/helloworld.py
@@ -0,0 +1,31 @@
+
+""" ``helloworld`` module.
+"""
+
+from wheezy.routing import url, Router
+
+
+def hello_world(environ, start_response):
+    start_response('200 OK', [
+        ('Content-type', 'text/html')
+    ])
+    return ["Hello World!"]
+
+
+r = Router()
+r.add_routes([
+    ('/', hello_world)
+])
+
+def main(environ, start_response):
+    handler, kwargs = r.match(environ['PATH_INFO'])
+    return handler(environ, start_response)
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    try:
+        print('Visit http://localhost:8080/')
+        make_server('', 8080, main).serve_forever()
+    except KeyboardInterrupt:
+        pass
+    print('\nThanks!')

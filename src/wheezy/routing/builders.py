9a284cf8f991823014ea11111ae646d2c18daac8
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-03 12:25:59 +0300

    Added try_build_regex_route build_route strategy.

--- src/wheezy/routing/builders.py
+++ src/wheezy/routing/builders.py
@@ -2,6 +2,7 @@ import re
 
 from route import Route
 from route import PlainRoute
+from route import RegexRoute
 
 
 RE_PLAIN_ROUTE = re.compile('^[\w/-]+$')
@@ -24,15 +25,25 @@ def try_build_plain_route(pattern, kwargs=None):
     return None
 
 
+def try_build_regex_route(pattern, kwargs=None):
+    """ There is no special tests to match regex selection
+        strategy.
+
+        >>> r = try_build_regex_route(r'abc')
+        >>> assert isinstance(r, RegexRoute)
+    """
+    return RegexRoute(pattern, kwargs)
+
+
 def build_route(pattern, kwargs, route_builders):
-    """ If ``pattern`` is an object drived from ``Route`` than it 
+    """ If ``pattern`` is an object drived from ``Route`` than it
         simply returned.
 
         >>> pattern = PlainRoute(r'abc')
         >>> r = build_route(pattern, None, [])
         >>> assert pattern == r
 
-        If ``pattern`` is a string than try to find 
+        If ``pattern`` is a string than try to find
         sutable route builder to create a route.
 
         >>> from config import route_builders
@@ -58,5 +69,3 @@ def build_route(pattern, kwargs, route_builders):
         else:
             raise LookupError("No matching route factory found")
     return route
-
-

c8b78a1f55772a16c194bee501d88ad7c520913b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-03 12:27:24 +0300

    Default route_builders include regex route build strategy.

--- src/wheezy/routing/config.py
+++ src/wheezy/routing/config.py
@@ -1,5 +1,11 @@
+
+""" ``config`` module.
+"""
+
 from builders import try_build_plain_route
+from builders import try_build_regex_route
 
 route_builders = [
-    try_build_plain_route
+    try_build_plain_route,
+    try_build_regex_route
 ]

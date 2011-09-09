31ce889bdafb43d2866a7bc7b186519e5cd18491
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-09 20:14:16 +0300

    Added pragma nocover

--- src/wheezy/routing/p2to3.py
+++ src/wheezy/routing/p2to3.py
@@ -7,15 +7,13 @@ import sys
 
 PY3 = sys.version_info[0] == 3
 
-if PY3:
+if PY3: #pragma: nocover
     import collections
-
     string_type = str
-
     def iscallable(obj):
         #return isinstance(obj, collections.Callable)
         return any("__call__" in klass.__dict__
-                    for klass in type(obj).__mro__)
-else:
+            for klass in type(obj).__mro__)
+else: #pragma: nocover
     string_type = basestring
     iscallable = callable

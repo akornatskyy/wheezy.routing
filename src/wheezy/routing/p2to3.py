b1bf298859e54f9465a82b2b7fef7991a0c3cbdb
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-11 12:34:26 +0300

    Fixed pep8 warnings.

--- src/wheezy/routing/p2to3.py
+++ src/wheezy/routing/p2to3.py
@@ -7,13 +7,14 @@ import sys
 
 PY3 = sys.version_info[0] == 3
 
-if PY3: #pragma: nocover
+if PY3:  # pragma: nocover
     import collections
     string_type = str
+
     def iscallable(obj):
         #return isinstance(obj, collections.Callable)
         return any("__call__" in klass.__dict__
             for klass in type(obj).__mro__)
-else: #pragma: nocover
+else:  # pragma: nocover
     string_type = basestring
     iscallable = callable

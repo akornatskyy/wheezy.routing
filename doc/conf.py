28933fdad679b84e53ccd0ce328b8891ac30a81a
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-09-11 15:17:13 +0300

    Minor cleanup.

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(
-	0, 
+	0,
 	os.path.abspath(os.path.join('..', 'src'))
 )
 
@@ -29,7 +29,10 @@ sys.path.insert(
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.doctest',
+    'sphinx.ext.coverage', 'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
